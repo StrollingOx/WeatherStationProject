af2c194d4b0947445aa706215d26172cee466c07
StrollingOx <StrollingOx@github.com>
2019-06-11 22:13:05 +0200

    Weatherstation test - Multiple station communicating with a sensor using events

--- WeatherStationProject/Program/Program.cs
+++ WeatherStationProject/Program/Program.cs
@@ -12,46 +12,22 @@ namespace Program
 {
     internal class Program
     {
-        private static WeatherStation _station;
-        private static Measurement _measurement;
         private static System.Timers.Timer _timer;
         
         public static void Main(string[] args)
         {
-            /*** Creating and populating WeatherStation ***/
-            _station = new WeatherStation();
-            PopulateStation();
+            WeatherStation s1= new WeatherStation("King's Cross Station");
+            WeatherStation s2 = new WeatherStation("Marylebone Station");
+            WeatherStation s3 = new WeatherStation("Fenchurch Street Station");
+            WeatherStation s4 = new WeatherStation("Liverpool Street Station");
             
-            /*** Extracting the list of sensors that meet the given condition ***/
-            Console.WriteLine("SearchAllSensors(..., s => ((TemperatureSensor) s).GetTemperature() >= 0.0) method test");
-            List<Sensor> sensorsWithTemperatureHigherThanZeroDegrees = 
-                _station.SearchAllSensors(
-                    (int) SensorType.TemperatureSensor,
-                    s => ((TemperatureSensor) s).GetTemperature() >= 0.0);
-            //Printing the data...
-            foreach (var sensor in sensorsWithTemperatureHigherThanZeroDegrees) Console.WriteLine(sensor.ToString());
-            
-            /*** Measurement class test***/
-            Console.WriteLine("\nMeasurement class test");
-            _measurement = new Measurement();
-            _measurement.AddRecord(new Sensor("testSensor1"), 0.0);                     //Sensor8
-            _measurement.AddRecord(new PressureSensor(), 100.0);                               //Sensor9
-            _measurement.AddRecord(new TemperatureSensor((int)DegreeScale.Celsius, 90));  //Sensor10
-
-            Double sensorMeasurement = _measurement.GetRecord("Sensor9_measure_2");
-            Console.WriteLine("DICTIONARY_TEST(Sensor9): "+sensorMeasurement);
-
-            sensorMeasurement = _measurement.GetRecord("testSensor1_measure_1");
-            Console.WriteLine("DICTIONARY_TEST(testSensor1): "+sensorMeasurement);
-            
-            sensorMeasurement = _measurement.GetRecord("Sensor10", 3);
-            Console.WriteLine("DICTIONARY_TEST(Sensor10): "+sensorMeasurement);
-           
-            /*** Event test ***/
-            Console.WriteLine("\nSensors event test");
+            /*** Multiple station event test ***/
             Sensor eventSensor = new HumiditySensor(); //Sensor11
             ((HumiditySensor)eventSensor).SetHumidity(38.52);
-            eventSensor.MeasurementRegistered += _station.OnMeasurementRegistered;
+            eventSensor.MeasurementRegistered += s1.OnMeasurementRegistered;
+            eventSensor.MeasurementRegistered += s2.OnMeasurementRegistered;
+            eventSensor.MeasurementRegistered += s3.OnMeasurementRegistered;
+            eventSensor.MeasurementRegistered += s4.OnMeasurementRegistered;
             eventSensor.RegisterCurrentMeasure();
             
             
@@ -87,60 +63,31 @@ namespace Program
         private static void OnTimedEvent(object source, ElapsedEventArgs e)
         {
             Console.WriteLine("Generating Report...");
-            GenerateReport();
+//            GenerateReport();
             Console.WriteLine("Success! Next report in {0}s.", _timer.Interval);
         }
-        private static void PopulateStation()
-        {
-            Sensor sensor1 = new Sensor("Awesome sensor");
-            Sensor sensor2 = new TemperatureSensor();
-            sensor2.Name = "OOF sensor";
-            Sensor sensor3 = new HumiditySensor();
-            Sensor sensor4 = new TemperatureSensor();
-            (sensor4 as TemperatureSensor).SetScale((int)DegreeScale.Fahrenheit);
-            Sensor sensor5 = new TemperatureSensor((int)DegreeScale.Celsius, 100);
-            Sensor sensor6 = new TemperatureSensor((int)DegreeScale.Celsius, 80);
-            Sensor sensor7 = new TemperatureSensor((int)DegreeScale.Celsius, -3);
-            
-            _station.AddSensor(sensor1);
-            _station.AddSensor(sensor2);
-            _station.AddSensor(sensor3);
-            _station.AddSensor(sensor4);
-            _station.AddSensor(sensor5);
-            _station.AddSensor(sensor6);
-            _station.AddSensor(sensor7);
-        }
-        private static void GenerateReport()
-        {
-            var someRam = new MemoryStream();
-            DataContractJsonSerializer serializer = new DataContractJsonSerializer(_station.GetType());
-            
-            serializer.WriteObject(someRam,_station);
-            someRam.Position = 0;
-            StreamReader sr = new StreamReader(someRam);
-            string jsonString = sr.ReadToEnd();
-
-            
-            var fileName = @"C:\Users\zai-2\Desktop\Reports\report"+
-                           DateTime.Now.ToString("--dd-MM-yyyyTHHmm")
-                           +".json";
-            Console.WriteLine(fileName);
-            
-            //TODO: Why are we using 'using' here?
-            using (StreamWriter file = File.CreateText(fileName))
-            {
-                file.Write(jsonString);
-            }
-        }
+        
+//        private static void GenerateReport()
+//        {
+//            var someRam = new MemoryStream();
+//            DataContractJsonSerializer serializer = new DataContractJsonSerializer(_station.GetType());
+//            
+//            serializer.WriteObject(someRam,_station);
+//            someRam.Position = 0;
+//            StreamReader sr = new StreamReader(someRam);
+//            string jsonString = sr.ReadToEnd();
+//
+//            
+//            var fileName = @"C:\Users\zai-2\Desktop\Reports\report"+
+//                           DateTime.Now.ToString("--dd-MM-yyyyTHHmm")
+//                           +".json";
+//            Console.WriteLine(fileName);
+//            
+//            //TODO: Why are we using 'using' here?
+//            using (StreamWriter file = File.CreateText(fileName))
+//            {
+//                file.Write(jsonString);
+//            }
+//        }
     }
 }
-
-/***** OLD BUT GOLD XD *****/
-//
-//            var serializer = new DataContractSerializer(station.GetType());
-//            var someRam = new MemoryStream();
-//            serializer.WriteObject(someRam, station);
-//            someRam.Seek(0, SeekOrigin.Begin);
-//            Console.WriteLine(
-//                XElement.Parse(
-//                Encoding.ASCII.GetString(someRam.GetBuffer()).Replace("\0", "")));
--- WeatherStationProject/Program/WeatherStation.cs
+++ WeatherStationProject/Program/WeatherStation.cs
@@ -16,78 +16,22 @@ namespace Program
 
     public class WeatherStation
     {
-        
-        [DataMember(Name = "List of sensor")] 
-        private readonly List<Sensor> _sensors;
-        
-
-        public WeatherStation()
+        private string _stationName;
+        public WeatherStation(String name)
         {
-            _sensors = new List<Sensor>();
-        }
-        public void AddSensor(Sensor sensor)
-        {
-            _sensors.Add(sensor);
-        }
-        public void GetAllTemperatures()
-        {
-            StringBuilder result = new StringBuilder("WeatcherStation -> Temperature sensors :\n");
-            foreach (Sensor sensor in _sensors)
-            {
-                if (sensor is ITemperature)
-                {
-                    result.Append(sensor.Name+": "
-                                      + (sensor as ITemperature).GetTemperature()+ " "
-                                      + (sensor as ITemperature).GetScaleSymbol()
-                                      +"\n");
-                }
-                
-            }
-            
-            Console.Write(result);
+            _stationName = name;
         }
         
-        public List<Sensor> SearchAllSensors(int sensorType, Func<Sensor , bool> condition)
-        {
-            //TODO: Optimize
-            var tSensors = new List<Sensor>();
-            var hSensors = new List<Sensor>();
-            var pSensors = new List<Sensor>();
-
-            foreach (var sensor in _sensors)
-            {
-                switch (sensor)
-                {
-                    case TemperatureSensor _:
-                        tSensors.Add(sensor);
-                        break;
-                    case HumiditySensor _:
-                        hSensors.Add(sensor);
-                        break;
-                    case PressureSensor _:
-                        pSensors.Add(sensor);
-                        break;
-                }
-            }
-            
-            switch(sensorType)
-            {
-                case 0: return tSensors.Where(condition).ToList();
-                case 1: return hSensors.Where(condition).ToList();
-                case 2: return pSensors.Where(condition).ToList();
-                default: return _sensors.Where(condition).ToList();
-            }
-        }
-
         public void OnMeasurementRegistered(object o, Measurement e)
         {
+            Console.WriteLine(_stationName +" - warning! An event occured.");
             Console.WriteLine("Object " + (o as Sensor)?.Name + " just registered new measure.");
             for (var i = 0; i < e.Counter; i++)
             {
                 var key = (o as Sensor)?.Name + "_measure_" + (i + 1);
                 var value = e.GetRecord((o as Sensor)?.Name + "_measure_" + (i + 1));
                 
-                Console.WriteLine( key + ": " + value);
+                Console.WriteLine( key + ": " + value +"\n");
             }
         }
     }
